2d96396a51b1d98177f2d45dd6081aaf76156025
Elber-Kevenny <vieirabeb@gmail.com>
2026-01-21 11:25:59 -0300

    react_autocomplete

---
+++ src/components/Autocomplete.tsx
@@ -0,0 +1,128 @@
+import React, { useMemo, useState } from 'react';
+import { Person } from '../types/Person';
+
+interface Props {
+  delay?: number;
+  person: Person[];
+  selectedPerson?: Person;
+  onSelected: (person: Person | null) => void;
+}
+
+function debounce<T extends (...args: Parameters<T>) => void>(
+  callback: T,
+  delay: number,
+) {
+  let timerId = 0;
+
+  return (...args: Parameters<T>) => {
+    window.clearTimeout(timerId);
+    timerId = window.setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+}
+
+export const Autocomplete: React.FC<Props> = ({
+  person,
+  delay = 300,
+  onSelected = () => {},
+  selectedPerson,
+}) => {
+  const [query, setQuery] = useState<string>(''); // caroulos
+  const [appliedQuery, setAppliedQuery] = useState('');
+
+  const [isActive, setIsActive] = useState<boolean>(false);
+
+  const handleFocus = () => {
+    setIsActive(true);
+  };
+
+  const handleBlur = () => {
+    setIsActive(false);
+  };
+
+  const applyQuery = useMemo(
+    () => debounce((value: string) => setAppliedQuery(value), delay),
+    [delay],
+  );
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value;
+
+    setQuery(newValue);
+    applyQuery(newValue);
+
+    if (newValue.length !== selectedPerson?.name.length) {
+      onSelected(null);
+    }
+  };
+
+  const suggestions = useMemo(() => {
+    const normalized = appliedQuery.trim().toLowerCase();
+
+    return person.filter(item => {
+      const nam = item.name.toLowerCase();
+
+      return nam.includes(normalized);
+    });
+  }, [appliedQuery, person]); //dependencias
+
+  return (
+    <>
+      <div className={`dropdown ${isActive ? 'is-active' : ''}`}>
+        <div className="dropdown-trigger">
+          <input
+            type="text"
+            placeholder="Enter a part of the name"
+            className="input"
+            data-cy="search-input"
+            value={query}
+            onChange={handleQueryChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          />
+        </div>
+        <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+          <div className="dropdown-content">
+            {suggestions.map((people, i) => (
+              <div
+                className="dropdown-item"
+                data-cy="suggestion-item"
+                key={i + 1}
+              >
+                <a
+                  href="#"
+                  className="has-text-link"
+                  onMouseDown={() => {
+                    onSelected(people);
+                    setIsActive(false);
+                    setQuery(people.name);
+                    setAppliedQuery(people.name);
+                  }}
+                >
+                  {people.name}
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      {suggestions.length === 0 && (
+        <div
+          className="
+            notification
+            is-danger
+            is-light
+            mt-3
+            is-align-self-flex-start
+          "
+          role="alert"
+          data-cy="no-suggestions-message"
+        >
+          <p className="has-text-danger">No matching suggestions</p>
+        </div>
+      )}
+    </>
+  );
+};
+// (onMouseDown → onBlur → onClick)
